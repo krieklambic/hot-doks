4e010c1dd922a8190b175117b3ecdf94c16fd0c8
Frederic Collin <frederic.collin@gmail.com>
2024-12-19 08:50:57 +0100

    upd

--- hot-doks-app/src/config.ts
+++ hot-doks-app/src/config.ts
@@ -1,6 +1,6 @@
 // Environment-specific configuration
 const config = {
-    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8080',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
 };
 
 export default config;
